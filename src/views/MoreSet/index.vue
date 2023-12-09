f3acc83e177530fdf93936356fae476e15de45ce
Rao ShuangZhi <69039557+geniusZhi666@users.noreply.github.com>
2023-12-09 17:29:35 +0800

    Update index.vue

--- src/views/MoreSet/index.vue
+++ src/views/MoreSet/index.vue
@@ -132,7 +132,7 @@ const jumpTo = (url) => {
 
       .logo {
         transform: translateY(-8%);
-        font-family: 'Streetwear', "Pacifico-Regular"，sans-serif;
+        font-family: "Pacifico", "Pacifico-Regular", sans-serif;
         padding-left: 22px;
         width: 100%;
         height: 260px;
